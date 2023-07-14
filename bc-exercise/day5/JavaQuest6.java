c8d6fb200d216d853140bbad2475ac0bb801bec3
chris.ng <chris.ng.hk@gmail.com>
2023-07-14 16:54:35 +0800

    day5

--- bc-exercise/day5/JavaQuest6.java
+++ bc-exercise/day5/JavaQuest6.java
@@ -8,7 +8,7 @@ public class JavaQuest6 {
     // for loop to print first 15 numbers in Fibonacci Sequence
     int a =1,b =0, c=0;
     System.out.print("1 ");
-    for (int i = 0; i < 20; i++) {
+    for (int i = 0; i < 15; i++) {
       c = a+ b;
       b = a;
       a = c;
